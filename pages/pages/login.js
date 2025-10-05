b0a7e87806d457f5ce19a690f3b1b46fd0ff18ba
TET-Max <terrythompson530@gmail.com>
2025-10-05 14:01:11 -0400

    Create login.js

---
+++ pages/pages/login.js
@@ -0,0 +1,10 @@
+import { supabase } from "../lib/supabaseClient";
+
+export default function Login() {
+  return (
+    <div style={{ padding: 40, fontFamily: "Arial" }}>
+      <h1>Login Page</h1>
+      <p>Supabase client connected successfully.</p>
+    </div>
+  );
+}
